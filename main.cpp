a40633ad006c48dd8c53be1b7fec5890df2d0785
ken <kenschlesiger@gmail.com>
2024-05-06 18:13:51 +0200

    somethings wrong

--- main.cpp
+++ main.cpp
@@ -19,10 +19,27 @@ int main(int argc, char *argv[])
 	// Get Map from txt file
 	map<int, TLE> satMap = readTlesFromFile(fileName.c_str());
 
-	//TLE tle = satMap[12345];	// Beispiel
+	TLE test_case_tle = satMap[88888];	
+	TLE sonate = satMap[59112];	
+    ECICoordinate POS;
+    ECICoordinate VEL;
+    SGP4Propagator propagator;
+    
+    printf("Test Case: \n");
+    propagator.setTle(test_case_tle);
+    for (int i = 0; i <= 1440 * 60; i += 360 * 60) {
+        propagator.calculatePositionAndVelocity(i, POS, VEL);
+        printf("Position: %f %f %f \n", POS.x, POS.y, POS.z);
+        printf("Velocity: %f %f %f \n \n", VEL.x, VEL.y, VEL.z);
 
-
-	// Aufgabe: Ergänzen Sie hier ihren Code
+    }
+    printf("Sonate: \n");
+    propagator.setTle(sonate);
+    for (int i = 0; i <= 1440 * 60; i += 360 * 60) {
+        propagator.calculatePositionAndVelocity(i, POS, VEL);
+        printf("Position: %f %f %f \n", POS.x, POS.y, POS.z);
+        printf("Velocity: %f %f %f \n", VEL.x, VEL.y, VEL.z);
+    }
 
 
 }
